aa5ee514fa4ed628259c61f127ca1827cbee9949
royyy <roygatere@gmail.com>
2024-11-14 22:50:27 -0800

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -199,7 +199,7 @@ public class MyFrame extends JFrame {
 
     public static void main(String[] args) {
         try {
-            // Set system look and feel
+            
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
